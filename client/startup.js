cb5673105e917a17276fe0cc4a839d7bc128e487
Dan Scanlon <dscanlonpa@gmail.com>
2012-11-10 23:46:53 -0500

    centered itemCandidate

--- client/startup.js
+++ client/startup.js
@@ -6,8 +6,9 @@ Meteor.startup(function () {
 ///////////////////////////////////////////////////////////////////////////////
 	// Create a new itemCandidate
 	itemCandidate = new ItemCandidate();
-	var nameView = new ItemNameView({ el: $('#itemNamePicker'), model: itemCandidate });
-	var imageView = new ItemImageView({ el: $('#itemImagePicker'), model: itemCandidate });
-	var captionView = new ItemCaptionView({ el: $('#itemCaptionPicker'), model: itemCandidate });
-	var priceView = new ItemPriceView({ el: $('#itemPricePicker'), model: itemCandidate });
+	
+  nameView = new ItemNameView({ el: $('#itemNamePicker'), model: itemCandidate });
+	imageView = new ItemImageView({ el: $('#itemImagePicker'), model: itemCandidate });
+	captionView = new ItemCaptionView({ el: $('#itemCaptionPicker'), model: itemCandidate });
+	priceView = new ItemPriceView({ el: $('#itemPricePicker'), model: itemCandidate });
 });
