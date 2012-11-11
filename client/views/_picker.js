d444f9803028d694d7c256f1b30d595897ed1923
justinmakaila <justinmakaila@gmail.com>
2012-11-10 21:21:31 -0500

    Working _picker.js file

--- client/views/_picker.js
+++ client/views/_picker.js
@@ -3,7 +3,14 @@
 
 */
 var view = {
-
+	render : function () {
+		var template = _.template( $('.picker').html() );
+		this.$el.html(template);
+	},
+	events : {
+		'click .next' : 'next',
+		'click .previous' : 'previous'
+	}
 };
 
 /* 
